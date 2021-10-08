d64c15ed4bcb8498ef3270434a3cc57e6c68e7ab
Abhishek Thory <abhithory@gmail.com>
2021-10-08 09:13:52 +0530

    added config

--- old_truffle-config.js
+++ old_truffle-config.js
@@ -1,26 +1,12 @@
 require('babel-register');
 require('babel-polyfill');
 
-require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider');
-
-
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" 
-    },
-    development: {
-      provider: function(){
-        return new HDWalletProvider(
-          process.env.MNEMONIC,
-          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
-        )
-      },
-      gasPrice: 25000000000,
       network_id: "*" 
     },
   },
