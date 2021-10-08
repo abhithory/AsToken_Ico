9becafd91bcd10473f5ab9ffb230887d633f5383
Abhishek Thory <abhithory@gmail.com>
2021-10-08 12:49:49 +0530

    Updates

---
+++ precache-manifest.ae32a3be153e101ee86174cb4a6fc320.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "116ae03c4893df288140",
+    "url": "/AsToken_Ico/static/css/main.042beedc.chunk.css"
+  },
+  {
+    "revision": "116ae03c4893df288140",
+    "url": "/AsToken_Ico/static/js/main.116ae03c.chunk.js"
+  },
+  {
+    "revision": "ad5bad7df959a9c36461",
+    "url": "/AsToken_Ico/static/css/1.510d57d4.chunk.css"
+  },
+  {
+    "revision": "ad5bad7df959a9c36461",
+    "url": "/AsToken_Ico/static/js/1.ad5bad7d.chunk.js"
+  },
+  {
+    "revision": "c552ce8af5d1daf63b0d",
+    "url": "/AsToken_Ico/static/js/runtime~main.c552ce8a.js"
+  },
+  {
+    "revision": "39d70e07a5e398524425b13fae54dd84",
+    "url": "/AsToken_Ico/index.html"
+  }
+];
